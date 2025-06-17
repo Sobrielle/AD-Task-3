fb594b52ba58b1bc9037503ee9882270007b8c5d
Sobrielle <sgscalalay@gmail.com>
2025-06-17 13:37:37 +0800

    fit(index): checking if mongoDb and PostgreSQL connected successfully

--- handlers/postgreChecker.handler.php
+++ handlers/postgreChecker.handler.php
@@ -4,7 +4,7 @@ $host = "host.docker.internal";
 $port = "5555";
 $username = "user";
 $password = "password";
-$dbname = "mydatabase";
+$dbname = "Database3";
 
 $conn_string = "host=$host port=$port dbname=$dbname user=$username password=$password";
 
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+define('HANDLER_PATH', __DIR__ . '/handlers');
+
+include_once HANDLER_PATH . '/mongodbChecker.handler.php';
+include_once HANDLER_PATH . '/postgreChecker.handler.php';
+?>
