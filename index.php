9f222639656c71bae7ca173ecf763bcdb25d8841
Sobrielle <sgscalalay@gmail.com>
2025-06-21 15:38:19 +0800

    fit(composer): installing dependencies

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
-define('HANDLER_PATH', __DIR__ . '/handlers');
+    define('HANDLER_PATH', __DIR__ . '/handlers');
 
-include_once HANDLER_PATH . '/mongodbChecker.handler.php';
-include_once HANDLER_PATH . '/postgreChecker.handler.php';
+    include_once HANDLER_PATH . '/mongodbChecker.handler.php';
+    include_once HANDLER_PATH . '/postgreChecker.handler.php';
 ?>
+
+
